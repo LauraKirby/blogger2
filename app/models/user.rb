0310316ecc9b46cd5a745eaf9a95f75a24eac0ba
LauraKirby <laurakirby26@gmail.com>
2015-10-27 11:00:45 -0700

    update routes for shallow routing, all user_controller_specs pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates :email, presence: true
+	has_many :posts, dependent: :destroy
+	has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,32 @@
 Rails.application.routes.draw do
 
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  resources :posts
-
   root to: "posts#index"
+
+  resources :users do 
+  	resources :posts, shallow: true
+  end 
+
+  get '/users/edit', to:  'users#edit'
+  get '/users/show', to:  'users#show'
 
 end
 
 
-#  Prefix Verb   URI Pattern               Controller#Action
-#     posts GET    /posts(.:format)          posts#index
-#           POST   /posts(.:format)          posts#create
-#  new_post GET    /posts/new(.:format)      posts#new
-# edit_post GET    /posts/:id/edit(.:format) posts#edit
-#      post GET    /posts/:id(.:format)      posts#show
-#           PATCH  /posts/:id(.:format)      posts#update
-#           PUT    /posts/:id(.:format)      posts#update
-#           DELETE /posts/:id(.:format)      posts#destroy
-#      root GET    /                         posts#index
+#    Prefix Verb   URI Pattern                         Controller#Action
+#          root GET    /                                   posts#index
+#    user_posts GET    /users/:user_id/posts(.:format)     posts#index
+#               POST   /users/:user_id/posts(.:format)     posts#create
+# new_user_post GET    /users/:user_id/posts/new(.:format) posts#new
+#     edit_post GET    /posts/:id/edit(.:format)           posts#edit
+#          post GET    /posts/:id(.:format)                posts#show
+#               PATCH  /posts/:id(.:format)                posts#update
+#               PUT    /posts/:id(.:format)                posts#update
+#               DELETE /posts/:id(.:format)                posts#destroy
+#         users GET    /users(.:format)                    users#index
+#               POST   /users(.:format)                    users#create
+#      new_user GET    /users/new(.:format)                users#new
+#     edit_user GET    /users/:id/edit(.:format)           users#edit
+#          user GET    /users/:id(.:format)                users#show
+#               PATCH  /users/:id(.:format)                users#update
+#               PUT    /users/:id(.:format)                users#update
+#               DELETE /users/:id(.:format)                users#destroy
