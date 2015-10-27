5149d70014fea1b65a50a7c4aad4efd7d79579c2
LauraKirby <laurakirby26@gmail.com>
2015-10-27 11:43:07 -0700

    add form_for to create new user, update corresponding controller logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,33 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@user = User.new
   end
 
+  def create 
+  	@user = User.create(user_params)
+  		if @user.save        
+  			redirect_to user_show
+  		else
+  			render :new
+  		end
+  end 
+
   def show
+
   end
 
   def edit
   end
 end
+
+
+private
+
+  def user_params
+  	params.require(:user).permit(
+  		:first_name,
+  		:email,
+  		:password
+  		)
+  end	
